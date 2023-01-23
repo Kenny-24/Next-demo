0746f3134bb8d625db5a40ea5ea021193a5d3543
Kenny <zhangkune24@gmail.com>
2023-01-23 19:23:11 +0800

    fix：dynamic route

--- clothes/app/todos/[todoId]/page.tsx
+++ clothes/app/todos/[todoId]/page.tsx
@@ -1,3 +1,28 @@
-export default function TodoPge() {
-  return <div className="text-purple-400 text-center pt-20 text-6xl">666</div>;
+import { Todo } from 'typings';
+
+type PageProps = {
+  params: {
+    todoId: string;
+  };
+};
+const fetchTodos = async (todoId: string) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${todoId}`,
+  );
+  const todo: Todo = await res.json();
+  return todo;
+};
+export default async function TodoPge({ params: { todoId } }: PageProps) {
+  const todo = await fetchTodos(todoId);
+  return (
+    <div className="bg-yellow-100 text-purple-400 text-center m-10 p-10 text-xl ">
+      <p>
+        #{todo.id}: {todo.title}
+      </p>
+      <p>Completed: {todo.completed ? 'Yes' : 'No'}</p>
+      <p className="border-t border-black mt-5 text-right">
+        By User: {todo.userId}
+      </p>
+    </div>
+  );
 }
