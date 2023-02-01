273005ff0ddafe44ea1b4536e603585263765d32
Kenny <zhangkune24@gmail.com>
2023-02-01 19:18:12 +0800

    fix

--- frontend-next/pages/api/auth/[...nextauth].ts
+++ frontend-next/pages/api/auth/[...nextauth].ts
@@ -22,22 +22,39 @@ export default async function auth(req:NextApiRequest, res:NextApiResponse) {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
     }),
     ],
+    secret: process.env.NEXTAUTH_SECRET,
+    callbacks: {
+      async signIn({ user, account, profile, email, credentials }) {
+        return true;
+      },
+      async session({ session, token, user }) {
+        const sessionUser = { ...session.user, ...user };
+  
+        return Promise.resolve({
+          ...session,
+          user: sessionUser,
+        });
+      },
+    },
+    debug: true,
     session: {
       // Set to jwt in order to CredentialsProvider works properly
       strategy: 'jwt',
     },
+  }
+  )
+}
   
-    // pages: {
-    //   signIn: '/auth/signin',
-    //   signOut: '/auth/signout',
-    //   error: '/auth/error', // Error code passed in query string as ?error=
-    //   verifyRequest: '/auth/verify-request', // (used for check email message)
-    //   newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
-    // }
-})}
+
 // export const authOptions: NextAuthOptions = {
 //   // your configs
 //   adapter: PrismaAdapter(prisma),
