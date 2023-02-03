9aa316b134a44726988e90055c98f1453ec958b4
Kenny <zhangkune24@gmail.com>
2023-02-03 20:11:58 +0800

    fix

--- frontend-next/app/chat/auth.tsx
+++ frontend-next/app/chat/auth.tsx
@@ -1,14 +1,11 @@
 'use client';
 import { signIn, signOut, useSession } from 'next-auth/react';
-import { getServerSession } from 'next-auth/next';
 import { useSearchParams } from 'next/navigation';
 
 export default function Auth() {
-  const searchParams = useSearchParams();
-  const callbackUrl: any = searchParams.get('callbackUrl');
   const { data } = useSession();
   console.log(8888, data);
-  console.log(666);
+  // 问题 可能是幽灵依赖openid-client版本的问题
   return (
     <div className="mt-20">
       <button
